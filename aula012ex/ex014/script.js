c3a6def28d50b9433d454dad1c6a365288165701
Jailson santos da silva <Jailsonsantos6672@gmail.com>
2025-05-12 11:47:24 -0300

    modelo completo

--- aula012ex/ex014/script.js
+++ aula012ex/ex014/script.js
@@ -1,1 +1,21 @@
-alert('ola!')
+function carregar(){
+    var msg = window.document.getElementById('msg')
+    var  img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+   //var hora = 22
+    msg.innerHTML = `Agora são ${hora} horas.`
+        if (hora >= 0 && hora < 12) {
+            //bom dia!
+            img.src = 'manhã.png'
+            document.body.style.background = '#7293a2'
+        } else if (hora >= 12 && hora <= 18){
+            //boa tarde!
+            img.src = 'tarde.png'
+            document.body.style.background = '#fcbc98'
+        } else {
+            // boa nite
+            img.scr = 'noite.png'
+            document.body.style.background = '#574247'
+        }
+}
